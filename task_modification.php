d0a31dac26d5b206863c16310202eb1dbfbf0e17
Thibault Defrance <thibault@debian>
2017-10-26 11:23:43 +0200

    -fini

--- task_modification.php
+++ task_modification.php
@@ -7,11 +7,11 @@ try {
 
 
 if (isset($_POST['task_name']) AND isset($_POST['description']) AND isset($_POST['deadline']) AND isset($_POST['niveau']) AND isset($_POST['accomplie'])) {
-    $req1= $bdd->prepare('UPDATE task SET name=?, description=?, deadline=?, level_id=?, accomplie=? WHERE id2=?');
-    $req1->execute(array($_POST['task_name'],$_POST['description'],$_POST['deadline'],$_POST['niveau'],$_POST['accomplie'],$_GET['id2']));
+    $req= $bdd->prepare('UPDATE task SET name=?, description=?, deadline=?, level_id=?, accomplished=? WHERE id2=?');
+    $req->execute(array($_POST['task_name'],$_POST['description'],$_POST['deadline'],$_POST['niveau'],$_POST['accomplie'],$_GET['id2']));
 
 }else {
-    $erreure1=["veillez remplir tout les champs"];
+    $erreure="veillez remplir tout les champs";
 }
 $getLevel=$bdd->query('SELECT * FROM level');
 $levels=$getLevel->fetchAll();
@@ -20,11 +20,6 @@ $data=$bdd->prepare('SELECT * FROM task WHERE id2=?');
 $data->execute(array($_GET['id2']));
 $data2=$data->fetch();
 
-/*print($_POST['name']);
-print($_POST['description']);
-print($_POST['deadline']);
-print($_POST['accomplie']);*/
-print($_POST['level_id']);
 ?>
 <!DOCTYPE html>
 <html>
@@ -37,7 +32,7 @@ print($_POST['level_id']);
         <a href="task.php">Retour à la page d'accueil</a>
         </h2>
         <form class="" action="" method="post">
-            <input type="text" name="task_name" value="<?php echo $data2['name'];?>" placeholder="nom de la tache">
+            <input type="text" name="task_name" value="<?php echo $data2['name']; ?>" placeholder="nom de la tache">
             <input type="textarea" name="description" value="<?php echo $data2['description'];?>" placeholder="description">
             <input type="date" name="deadline" value="<?php echo $data2['deadline'];?>" placeholder="deadline yyyy-mm-jj">
             <select class="" name="niveau">
@@ -50,8 +45,6 @@ print($_POST['level_id']);
                    }
                  ?>
             </select>
-
-
             <p>accomplie</p><br>
             <p>oui</p>
             <input type="radio" name="accomplie" value="oui">
@@ -60,9 +53,9 @@ print($_POST['level_id']);
 
             <button type="submit" name="button">modifier</button>
             <div class="alert-danger">
-                <?php if (isset($erreure1)) {
-                    echo "veillez remplir tout les champs";
-                } ?>
+                <?php
+                    echo $erreure;
+                ?>
             </div>
         </form>
     </body>
